fc5db906667c71ba4da385f2ab08c11f9e26d2bc
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2025-02-03 15:36:50 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -17,9 +17,10 @@ payload = {
     "email": email
 }
 
-# Header-ul pentru cererea POST
+# Header-ul pentru cererea POST, inclusiv User-Agent
 headers = {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
 # Efectuarea cererii POST
